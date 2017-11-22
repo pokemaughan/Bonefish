f536ca9cc5ccd5b1f0a6eda1bb9e51d361bdd8e5
Mahallon <markmahlon@gmail.com>
2017-11-22 13:05:00 -0700

    parsing the bigger file for quality and saving the output with quality score 20 and 30

--- fastqQualityParse.py
+++ fastqQualityParse.py
@@ -1,7 +1,18 @@
 from Bio import SeqIO
-count = 0
-good_reads = (rec for rec in \
-              SeqIO.parse("pac-bf_lane3_reads-ACTAGGAG.fastq", "fastq")\
-              if min(rec.letter_annotations["phred_quality"])>=20)
-count = SeqIO.write(good_reads, "good_quality.fastq","fastq")
-print("Saved %i reads" % count)
+
+def qualityParse(filename,quality):
+    count = 0
+    good_reads = (rec for rec in \
+              SeqIO.parse(filename, "fastq")\
+              if min(rec.letter_annotations["phred_quality"])>=quality)
+    count = SeqIO.write(good_reads, "good_quality.fastq","fastq")
+    print("Saved %i reads" % count)
+
+def main():
+    qualityParse("pac-bf_lane3_reads-AGATACAA.fastq",30)
+
+if __name__=='__main__':
+    main()
+
+
+
